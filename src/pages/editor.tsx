9356905493e6a1198419feb8749e369bf7f1f321
fpxwe85213-admin <fpxwe85213@yahoo.co.jp>
2022-12-13 21:24:43 +0900

    IndexDBに保存できるように修正

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -2,16 +2,30 @@ import * as React from 'react'
 import styled from 'styled-components'
 import { useStateWithStorage } from '../hooks/use_state_with_storage'
 import * as ReactMarkdown from 'react-markdown'
+import { putMemo } from '../indexeddb/memos'
+import { Button } from '../components/button'
+import { SaveModal } from '../components/save_modal'
+
+const { useState } = React
 
 const Header = styled.header`
+  align-content: center;
+  display: flex;
   font-size: 1.5rem;
   height: 2rem;
+  justify-content: space-between;
   left: 0;
   line-height: 2rem;
   padding: 0.5rem 1rem;
   position: fixed;
   right: 0;
   top: 0;
+`
+
+const HeaderControl = styled.div`
+height: 2rem;
+display: flex;
+align-content: center;
 `
 
 const Wrapper = styled.div`
@@ -50,10 +64,17 @@ const StorageKey = 'pages/editor:text'
 export const Editor: React.FC = () => {
   const [text, setText] = useStateWithStorage('', StorageKey)
 
+  const [showModal, setShowModal] = useState(false)
+
   return (
     <>
       <Header>
         Markdown Editor
+        <HeaderControl>
+          <Button onClick={() => setShowModal(true)}>
+            保存する
+          </Button>
+        </HeaderControl>
       </Header>
       <Wrapper>
         <TextArea
@@ -64,6 +85,15 @@ export const Editor: React.FC = () => {
           <ReactMarkdown>{text}</ReactMarkdown>
         </Preview>
       </Wrapper>
+      {showModal && (
+        <SaveModal
+          onSave={(title: string): void => {
+            putMemo(title, text)
+            setShowModal(false)
+          }}
+          onCancel={() => setShowModal(false)}
+        />
+      )}
     </>
   )
 }
